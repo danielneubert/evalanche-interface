79e7487965cd4d521df0e26efc12398b910134ff
Daniel Neubert <git@danielneubert.com>
2020-11-13 08:49:11 +0100

    Add ProfileConnector

---
+++ src/Connectors/ProfileConnector.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Neubert\EvalancheInterface\Connectors;
+
+use Neubert\EvalancheInterface\Collections\Profiles\Profile;
+use Neubert\EvalancheInterface\Support\HashMap;
+
+/**
+ */
+class ProfileConnector extends Connector
+{
+    /**
+     * Client Accessor
+     *
+     * @var string
+     */
+    protected $clientAccessor = 'Profile';
+
+    // Documentation Missing
+    public function get() : Profile
+    {
+        return new Profile($this->getClient()->getById($this->_id()), 'Profile', $this);
+    }
+
+    // Documentation Missing
+    public function update(array $fields)
+    {
+        return $this->getClient()->updateById($this->_id(), HashMap::compose($fields));
+    }
+
+    // Documentation Missing
+    public function delete() : bool
+    {
+        return $this->getClient()->delete([$this->_id()]);
+    }
+}
